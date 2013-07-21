bf15ad1c71b3d6d39c8980bf8fa43a76da3b88ea
abhaskaran <7.ashwini@gmail.com>
2013-07-21 20:34:15 +0530

    Added necessary comments in HorseClass
    Added necessary comments in HorseClass

--- src/org/ashwini/basics/Horse.java
+++ src/org/ashwini/basics/Horse.java
@@ -1,13 +1,19 @@
+/**
+ * This project is to learn Java using HorseClass 
+ * as an example and the project is hosted on Github
+ * 
+ */
 package org.ashwini.basics;
 
+//Beginning of HorseClass
 public class Horse {
-
+//Fields/Instance variables of HorseClass
 	String color;
 	String purpose;
 	String breed;
 	int height;
 	int weight;
-	
+//Default constructor for HorseClass	
 	Horse(){
 		this.breed = "Arabian Quarter";
 		this.color = "Black";
